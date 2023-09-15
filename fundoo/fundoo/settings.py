10468630170cf106abc7c1fd528be885992043eb
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2023-09-15 15:33:27 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -108,11 +108,6 @@ DATABASES = {
         'PASSWORD': 'admin123',
         'HOST': 'database-1.ccloafkmlils.ap-south-1.rds.amazonaws.com',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-            'charset': 'utf8mb4',
-            "autocommit": True,
-        },
     }
 }
 #sagar kadam
